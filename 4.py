d1a5aa4ce083b1c5591636f1e1ff53e7ab1acc21
Amir <160425270+amirmohanejad@users.noreply.github.com>
2024-03-10 23:21:57 +0330

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 2
+b = 3
+
+v = a+b
+w = a-b
+x = a*b
+y = a/b
+z = a**b
+
+
+# In[ ]:
+
+
+print("Jam is ", v, "    Menha is ", w, "    Zarb is ", x, "    Taqsim is ", y, "    Tavan is ", z)
+input()
+
